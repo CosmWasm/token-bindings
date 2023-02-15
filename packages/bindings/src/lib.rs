cedd443b136ba1d44f999b02a8b5b447c6c17b98
Kerber0x <kerber0x@protonmail.com>
2023-02-15 09:49:49 +0000

    Making DenomUnit public so all Metadata fields can be set

--- packages/bindings/src/lib.rs
+++ packages/bindings/src/lib.rs
@@ -9,7 +9,7 @@ pub use query::{
     AdminResponse, DenomsByCreatorResponse, FullDenomResponse, MetadataResponse, ParamsResponse,
     TokenFactoryQuery, TokenQuery,
 };
-pub use types::{Metadata, Params};
+pub use types::{DenomUnit, Metadata, Params};
 
 // This is a signal, such that any contract that imports these helpers will only run on
 // blockchains that support token_factory feature
